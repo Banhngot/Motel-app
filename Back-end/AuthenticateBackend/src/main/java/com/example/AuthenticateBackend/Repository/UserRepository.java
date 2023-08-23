0ba799ee45efe77bc086c57d93713ab9faf9620c
Kachirou <hoang286272@gmail.com>
2023-08-23 08:44:59 +0700

    up

---
+++ Back-end/AuthenticateBackend/src/main/java/com/example/AuthenticateBackend/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.AuthenticateBackend.Repository;
+
+import com.example.AuthenticateBackend.Modles.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<ApplicationUser,Integer>{
+    Optional<ApplicationUser> findByUsername(String username);
+}
