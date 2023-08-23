5e1d2fc78f0c024796a58f43f64a0890f5e441d2
Kachirou <hoang286272@gmail.com>
2023-08-23 09:44:57 +0700

    up

---
+++ Back-end/AuthenticateBackend/src/main/java/com/example/AuthenticateBackend/Controller/AuthenticationController.java
@@ -0,0 +1,19 @@
+package com.example.AuthenticateBackend.Controller;
+
+import com.example.AuthenticateBackend.Modles.ApplicationUser;
+import com.example.AuthenticateBackend.Modles.RegistrationDTO;
+import com.example.AuthenticateBackend.Service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationService authenticationService;
+    @PostMapping("/register")
+    public ApplicationUser registerUser(@RequestBody RegistrationDTO body){
+        return authenticationService.registerUser(body.getUsername(),body.getPassword());
+    }
+}
