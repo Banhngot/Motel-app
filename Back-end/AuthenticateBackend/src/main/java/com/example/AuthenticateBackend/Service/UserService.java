c4916ddefa78c3b1c6104c597add72f3ec6027c0
Kachirou <hoang286272@gmail.com>
2023-08-23 08:27:27 +0700

    up

---
+++ Back-end/AuthenticateBackend/src/main/java/com/example/AuthenticateBackend/Service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.AuthenticateBackend.Service;
+
+import com.example.AuthenticateBackend.Modles.ApplicationUser;
+import com.example.AuthenticateBackend.Modles.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder encoder;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("In the user details service");
+        if(!username.equals("Hoang")) throw new UsernameNotFoundException("Not Hoang");
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(1,"Admin"));
+        return new ApplicationUser(1,"Hoang",encoder.encode("password"),roles);
+    }
+}
